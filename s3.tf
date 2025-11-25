3d9f364d7e69b1468bbd57ddb281a83b4e8b8a25
Pratik Das <pratik.x.das@gmail.com>
2025-11-25 00:08:26 -0600

    Updating S3 Bucket Object Ownership

--- s3.tf
+++ s3.tf
@@ -16,7 +16,7 @@ resource "aws_s3_bucket" "this" {
 resource "aws_s3_bucket" "log_bucket" {
   depends_on = [aws_s3_bucket.this]
   bucket     = var.s3_log_bucket
-  acl        = "log-delivery-write" # Grants log delivery group write permissions
+  # acl        = "log-delivery-write" # Grants log delivery group write permissions
 }
 
 resource "aws_s3_bucket_logging" "source_bucket_logging" {
@@ -89,8 +89,8 @@ resource "aws_s3_bucket_ownership_controls" "this" {
   for_each   = var.name
   bucket     = aws_s3_bucket.this[each.key].id
   rule {
-    # object_ownership = "BucketOwnerPreferred"
-    object_ownership = "BucketOwnerEnforced"
+    object_ownership = "BucketOwnerPreferred"
+    # object_ownership = "BucketOwnerEnforced"
   }
 }
 
@@ -102,7 +102,8 @@ resource "aws_s3_bucket_policy" "this" {
 }
 
 resource "aws_s3_bucket_acl" "s3_acl" {
-  depends_on = [aws_s3_bucket_policy.this]
+  # depends_on = [aws_s3_bucket_policy.this]
+  depends_on = [aws_s3_bucket_ownership_controls.this]
   for_each   = var.name
   bucket     = aws_s3_bucket.this[each.key].id
   acl        = "private"
